b4d53435c88fc495a5d199449ae7373b2a13c312
w33zy <wzy@outlook.com>
2017-01-18 11:18:28 -0500

    finished adding devise

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -1,5 +1,5 @@
 class ForumThreadsController < ApplicationController
-  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_forum_thread, except: [:index, :new, :create]
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravatar_url(email, size)
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    default_url = "#{asset_url 'avatars/avatar.png'}"
+    url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
+  end
 end
